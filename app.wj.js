911855f323fce0ffe955d4b340c538fb827496f6
harley-travis <harley.travis2@gmail.com>
2018-05-12 15:55:24 -0600

    init the wj script and frontend end user site

---
+++ app.wj.js
@@ -0,0 +1,33 @@
+var WJ = {
+    Init: function() {
+    
+    },
+    Widget: { 
+        GetJobs: function() {
+            $.ajax({
+                url: 'http://localhost:3000/api/jobs/getAllJobs', // need to include the company id as a parameter
+                type: 'GET',
+                contentType: "application/json",
+                dataType: "json",
+                crossDomain: true,
+                success: function(data) {
+
+                    $('#wj').html('<table id="wj-table" class="table table-hover"><tr><th scope="col">Position</th><th scope="col">Department</th><th scope="col">Location</th><th scope="col"></th></tr>');
+                        
+                    for(var i = 0; i < data.length; i++) {
+                        var job = data[i];
+                        var html = "<tr><td>"+job.title+"</td><td>"+job.department+"</td><td>"+job.location+"</td><td><a href='#' class='btn btn-success btn-sm'>View Job</a></td></tr>"
+                        $("#wj-table").find('tbody').append(html);
+                    }
+                },
+                error: function() {
+                    console.log('nope');
+                }
+            });
+        }
+    }
+}
+
+$(document).ready(function(){
+    WJ.Widget.GetJobs();
+});
